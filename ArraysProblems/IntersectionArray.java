41c2fc2c7c25d79f4403cc7b58b62a2f93b3bc9b
mr.jashwanthreddy <89520760+JashwanthReddy13189@users.noreply.github.com>
2025-06-05 18:38:08 +0530

    Update IntersectionArray.java

--- ArraysProblems/IntersectionArray.java
+++ ArraysProblems/IntersectionArray.java
@@ -9,6 +9,31 @@ public class IntersectionArray {
 
         // Brute Force
         bruteForceIntersection(arr1, arr2);
+
+        // Optimal Approach
+        optimalApproachIntersection(arr1, arr2);
+    }
+
+    private static void optimalApproachIntersection(int[] arr1, int[] arr2) {
+        int n1 = arr1.length, n2 = arr2.length;
+        int i = 0, j = 0;
+        ArrayList<Integer> intersection = new ArrayList<>();
+        while (i < n1 && j < n2) {
+            if (arr1[i] < arr2[j]) {
+                i++;
+            } else if (arr2[j] < arr1[i]) {
+                j++;
+            } else {
+                intersection.add(arr1[i]);
+                i++;
+                j++;
+            }
+        }
+        System.out.print("[");
+        for (int m : intersection) {
+            System.out.print(m + " ");
+        }
+        System.out.print("]");
     }
 
     private static void bruteForceIntersection(int[] arr1, int[] arr2) {
@@ -22,12 +47,12 @@ public class IntersectionArray {
                     vis[j] = 1;
                     break;
                 }
-                if (arr2[j] > arr1[i]) return;
+                if (arr2[j] > arr1[i]) break;
             }
         }
         System.out.print("[");
-        for (int m: intersection) {
-            System.out.print(m + ", ");
+        for (int m : intersection) {
+            System.out.print(m + " ");
         }
         System.out.print("]");
     }
